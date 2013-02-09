4ed1dd7e5a3687ca43df470dacdf39528b40bc66
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2013-02-09 09:55:02 +0100

    first simple acceptance test

---
+++ spec/acceptance/root_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature 'Home page' do
+
+  scenario 'Visit home page' do
+    visit_home_page
+    see_home_page
+  end
+
+  def visit_home_page
+    visit '/'
+  end
+
+  def see_home_page
+    page.should have_selector('h1', text: 'PEP Germany Campaigns')
+  end
+
+end
