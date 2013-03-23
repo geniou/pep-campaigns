2b1f0f71babe39876a2434c48dfa9fb4f9971f63
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2013-03-23 11:07:37 +0100

    fix rate styling

--- app/models/question/rate.rb
+++ app/models/question/rate.rb
@@ -2,7 +2,13 @@ class Question
   class Rate < Question
 
     def input_element
-      [:numeric_value, { label: text, as: :radio, collection: [1,2,3,4,5] }]
+      [:numeric_value,
+        { label: text,
+          as: :radio,
+          collection: [1,2,3,4,5],
+          wrapper_html: { class: 'rate'}
+        }
+      ]
     end
   end
 end
