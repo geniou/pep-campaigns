d1afb8221f33dd337246920defcd4341ab72bf50
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2013-05-24 19:23:12 +0200

    remove duplicated code

--- app/models/question/simple_text.rb
+++ app/models/question/simple_text.rb
@@ -1,29 +1,8 @@
 class Question
-  class SimpleText < Question
-
-    def self.model_name
-      Question.model_name
-
-    end
-    def field_name
-      :text_value
-    end
+  class SimpleText < Question::Text
 
     def input_type
       :string
-    end
-
-    def input_valid?(value)
-      !value.blank? || !required
-    end
-
-    def summary_type
-      :list
-    end
-
-    def summary(answers)
-      answers
-        .reject { |answer| answer.value.blank? }
     end
   end
 end
