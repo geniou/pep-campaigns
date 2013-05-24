82fe588416db96d96c44e0d147867108656f38ab
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2013-05-24 17:12:45 +0200

    summary styling

--- app/helpers/summary_helper.rb
+++
@@ -1,9 +0,0 @@
-module SummaryHelper
-
-  def summary_number(number)
-    css_class = 'rate important' if number > 3.5
-    content_tag(:span, class: css_class) do
-      number_with_precision(number)
-    end
-  end
-end
